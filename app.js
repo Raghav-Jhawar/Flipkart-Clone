ae6f8a71be847846dd9f35390d3fdd102ed7e86d
Raghav <shivjhawar64@gmail.com>
2021-05-29 13:43:31 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended : true}));
+app.use("/static",express.static("static"));
+
+app.set('view engine', 'ejs'); // Set the template engine as pug
+app.set('views', path.join(__dirname, 'views')); // Set the views directory
+
+// End Points
+
+app.get("/",(req,res)=>{
+    res.status(200).render("index.ejs");
+});
+
+app.get("/login",(req,res)=>{
+    res.render("login.ejs");
+});
+
+
+
+app.listen(2000, ()=>{
+    console.log("the app is listening at https://localhost:2000");
+})
